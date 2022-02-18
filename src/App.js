6e3f8632eddc8760fb9228135f86c85a82db0998
David Chan <dchaan1@gmail.com>
2022-02-17 17:34:52 -0800

    Refactor form

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import './App.css';
+import React from 'react';
+import NewExpense from './components/NewExpense/NewExpense';
 import Expenses from './components/Expenses/Expenses';
 
 const App = () => {
@@ -26,7 +27,8 @@ const App = () => {
 
   return (
     <div>
-      <Expenses items={expenses}/>
+      <NewExpense />
+      <Expenses items={expenses} />
     </div>
   );
 }
